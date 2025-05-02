a83648cf3a57a0381ef5d435ad5731eca1657998
Ahmed Omar <omar155432@gmai.com>
2025-05-02 19:19:14 +0300

    adding sliding animation to splash view

---
+++ lib/Features/splash/presention/views/widgets/sliding_text.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SlidingText extends StatelessWidget {
+  const SlidingText({
+    super.key,
+    required this.animation,
+  });
+
+  final Animation<Offset> animation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(animation: animation,builder: (context, child)  =>
+       SlideTransition(position:animation ,
+        child:const Text('Read Free Books',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20,
+            )),
+      ),
+    );
+  }
+}
--- lib/Features/splash/presention/views/widgets/spalsh_view_body.dart
+++ lib/Features/splash/presention/views/widgets/spalsh_view_body.dart
@@ -1,9 +1,33 @@
 import 'package:bookly/Core/utils/assets.dart';
+import 'package:bookly/Features/splash/presention/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
 
-class SpalshViewBody extends StatelessWidget {
+class SpalshViewBody extends StatefulWidget {
   const SpalshViewBody({super.key});
 
+  @override
+  State<SpalshViewBody> createState() => _SpalshViewBodyState();
+}
+
+class _SpalshViewBodyState extends State<SpalshViewBody> with SingleTickerProviderStateMixin {
+  @override
+  
+ late AnimationController animationController;
+ late Animation<Offset> animation;
+
+ @override
+ void initState() {
+    animationController=AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    animation=Tween<Offset>(begin:const  Offset(0,2),end: const Offset(0,0)).animate(animationController);
+    animationController.forward();
+    super.initState();
+  }
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+ 
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -11,11 +35,7 @@ class SpalshViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AssetsData.logo),
-       const Text('Read Free Books',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 20,
-            )),
+        SlidingText(animation: animation),
       ],
     );
   }
