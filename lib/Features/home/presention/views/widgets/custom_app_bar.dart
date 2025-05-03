08a053c80de737af824dc820e6658a686b035c07
Ahmed Omar <omar155432@gmai.com>
2025-05-03 20:55:19 +0300

    finish App Bar in home view

---
+++ lib/Features/home/presention/views/widgets/custom_app_bar.dart
@@ -0,0 +1,30 @@
+import 'package:bookly/Core/utils/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image.asset(
+            AssetsData.logo,
+            height: 120,
+            width: 120,
+          ),
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                FontAwesomeIcons.magnifyingGlass,
+                size: 25,
+              ))
+        ],
+      ),
+    );
+  }
+}
