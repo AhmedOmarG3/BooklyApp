dd40330985da9d5bff7ae0060d70508fd544b5a8
Ahmed Omar <omar155432@gmai.com>
2025-06-17 00:51:16 +0300

    Best cubit done

--- lib/Features/home/presention/manager/best_seller_books_cubit/best_seller_cubit_cubit.dart
+++ lib/Features/home/presention/manager/best_seller_books_cubit/best_seller_cubit_cubit.dart
@@ -1,8 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/Features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'best_seller_cubit_state.dart';
 
 class BestSellerCubitCubit extends Cubit<BestSellerCubitState> {
-  BestSellerCubitCubit() : super(BestSellerCubitInitial());
+  BestSellerCubitCubit(this.homeRepo) : super(BestSellerCubitInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> fetchBestSellerBooks() async {
+    emit(BestSellerLoadingState());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold((failure) {
+      emit(BestSellerFailureState(failure.message));
+    }, (books) {
+      emit(BestSellerSuccessState(books));
+    });
+  }
 }
--- lib/Features/home/presention/manager/best_seller_books_cubit/best_seller_cubit_state.dart
+++ lib/Features/home/presention/manager/best_seller_books_cubit/best_seller_cubit_state.dart
@@ -8,3 +8,12 @@ sealed class BestSellerCubitState extends Equatable {
 }
 
 final class BestSellerCubitInitial extends BestSellerCubitState {}
+final class BestSellerLoadingState extends BestSellerCubitState {}
+final class BestSellerSuccessState extends BestSellerCubitState {
+  final List<BookModel> books;
+  const BestSellerSuccessState(this.books);
+}
+final class BestSellerFailureState extends BestSellerCubitState {
+  final String errMessage;
+  const BestSellerFailureState(this.errMessage);
+}
