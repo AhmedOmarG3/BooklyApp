265774f59b0835e56dacdf6aeb48726084b9503c
Ahmed Omar <omar155432@gmai.com>
2025-06-17 20:53:50 +0300

    finish featured list view display

--- lib/Features/home/presention/views/widgets/list_view_item.dart
+++ lib/Features/home/presention/views/widgets/list_view_item.dart
@@ -1,25 +1,33 @@
-import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
 class ListViewItem extends StatelessWidget {
-  const ListViewItem( {super.key, required this.imageUrl});
-final String imageUrl;
+  const ListViewItem({super.key, required this.imageUrl});
+  final String imageUrl;
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(right: 8),
       child: SizedBox(
-        child: AspectRatio(
-          aspectRatio: 2.5 / 4,
-          child: Container(
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: Colors.white,
-                image:  DecorationImage(
-                    fit: BoxFit.fill,
-                    image: NetworkImage(
-                      imageUrl??'https://www.noor-book.com/publice/covers_cache_webp/2/8/b/9/0e368e83428b9cbd70f7a502d2c1b3e3.jpg.webp'))),
-          ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(16),
+          child: AspectRatio(
+              aspectRatio: 2.5 / 4,
+              child: CachedNetworkImage(
+                  placeholder: (context, url) => Shimmer.fromColors(
+  baseColor: Colors.grey.shade300,
+  highlightColor: Colors.grey.shade100,
+  child: Container(
+    decoration: BoxDecoration(
+      color: Colors.grey.shade300,
+      borderRadius: BorderRadius.circular(16), 
+    ),
+  ),
+),
+                  fit: BoxFit.fill,
+                  imageUrl: imageUrl ,errorWidget: (context, url, error) => const Icon(Icons.error),
+                      )),
         ),
       ),
     );
